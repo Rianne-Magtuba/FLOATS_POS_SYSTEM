9530be76c93b207e399d27e38c7e71b202977815
jes <jesterresma@gmail.com>
2025-07-02 06:07:32 +0800

    commit

---
+++ FLOATS_POS_SYSTEM/CustomMessageBox.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CustomMessageBox
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        lbl_msg = New Label()
+        btn_yes = New Button()
+        btn_no = New Button()
+        btn_ok = New Button()
+        SuspendLayout()
+        ' 
+        ' lbl_msg
+        ' 
+        lbl_msg.AutoSize = True
+        lbl_msg.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lbl_msg.Location = New Point(16, 15)
+        lbl_msg.MinimumSize = New Size(270, 65)
+        lbl_msg.Name = "lbl_msg"
+        lbl_msg.Size = New Size(270, 65)
+        lbl_msg.TabIndex = 0
+        lbl_msg.Text = "Label"
+        lbl_msg.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' btn_yes
+        ' 
+        btn_yes.BackColor = Color.Black
+        btn_yes.FlatStyle = FlatStyle.Flat
+        btn_yes.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn_yes.ForeColor = Color.White
+        btn_yes.Location = New Point(40, 90)
+        btn_yes.Name = "btn_yes"
+        btn_yes.Padding = New Padding(1)
+        btn_yes.Size = New Size(100, 40)
+        btn_yes.TabIndex = 5
+        btn_yes.Text = "Yes"
+        btn_yes.UseVisualStyleBackColor = False
+        btn_yes.Visible = False
+        ' 
+        ' btn_no
+        ' 
+        btn_no.BackColor = Color.Black
+        btn_no.FlatStyle = FlatStyle.Flat
+        btn_no.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn_no.ForeColor = Color.White
+        btn_no.Location = New Point(161, 90)
+        btn_no.Name = "btn_no"
+        btn_no.Padding = New Padding(1)
+        btn_no.Size = New Size(100, 40)
+        btn_no.TabIndex = 6
+        btn_no.Text = "No"
+        btn_no.UseVisualStyleBackColor = False
+        btn_no.Visible = False
+        ' 
+        ' btn_ok
+        ' 
+        btn_ok.BackColor = Color.Black
+        btn_ok.FlatStyle = FlatStyle.Flat
+        btn_ok.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn_ok.ForeColor = Color.White
+        btn_ok.Location = New Point(99, 90)
+        btn_ok.Name = "btn_ok"
+        btn_ok.Padding = New Padding(1)
+        btn_ok.Size = New Size(100, 40)
+        btn_ok.TabIndex = 7
+        btn_ok.Text = "OK"
+        btn_ok.UseVisualStyleBackColor = False
+        btn_ok.Visible = False
+        ' 
+        ' CustomMessageBox
+        ' 
+        AutoScaleDimensions = New SizeF(9F, 23F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Black
+        ClientSize = New Size(302, 153)
+        Controls.Add(btn_ok)
+        Controls.Add(btn_no)
+        Controls.Add(btn_yes)
+        Controls.Add(lbl_msg)
+        Font = New Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ForeColor = Color.White
+        MaximizeBox = False
+        MdiChildrenMinimizedAnchorBottom = False
+        MinimizeBox = False
+        Name = "CustomMessageBox"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "CustomMessageBox"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lbl_msg As Label
+    Friend WithEvents btn_yes As Button
+    Friend WithEvents btn_no As Button
+    Friend WithEvents btn_ok As Button
+End Class
---
+++ FLOATS_POS_SYSTEM/OrderDetails.vb
@@ -0,0 +1,226 @@
+﻿Imports Microsoft.Data.SqlClient
+
+Public Class OrderDetails
+
+    Private sortAscending As Boolean = True
+    Private hasSortedOnce As Boolean = False
+    Private currentTable As DataGridView
+
+    Private Sub OrderDetails_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        currentTable = dgvProducts
+        GetAllProducts()
+    End Sub
+
+    Private Sub GetAllProducts()
+
+        Dim connectionString As String = "Data Source=DESKTOP-FOAVQMH\SQLEXPRESS; Initial Catalog=FLOATS_POS_SYSTEM; Integrated Security=True; TrustServerCertificate=True;"
+
+        Dim selectQuery As String = "SELECT product_id, category, item_name, size, price FROM Products"
+
+        Using connection As New SqlConnection(connectionString)
+            connection.Open()
+
+            Using command As New SqlCommand(selectQuery, connection)
+                Using reader As SqlDataReader = command.ExecuteReader()
+                    dgvProducts.Rows.Clear()
+
+                    While reader.Read()
+                        dgvProducts.Rows.Add(
+                            Int(reader("product_id")),
+                            reader("category").ToString(),
+                            reader("item_name").ToString(),
+                            reader("size").ToString(),
+                            (FormatCurrency(reader("price")).Replace("$", "₱ "))
+                        )
+                    End While
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Sub SortAllProducts()
+
+        Dim connectionString As String = "Data Source=DESKTOP-FOAVQMH\SQLEXPRESS; Initial Catalog=FLOATS_POS_SYSTEM; Integrated Security=True; TrustServerCertificate=True;"
+
+        Dim selectQuery As String
+
+        If (sortAscending) Then
+            selectQuery = "SELECT product_id, category, item_name, size, price FROM Products ORDER BY product_id ASC"
+        Else
+            selectQuery = "SELECT product_id, category, item_name, size, price FROM Products ORDER BY product_id DESC"
+        End If
+
+        Using connection As New SqlConnection(connectionString)
+            connection.Open()
+
+            Using command As New SqlCommand(selectQuery, connection)
+                Using reader As SqlDataReader = command.ExecuteReader()
+                    dgvProducts.Rows.Clear()
+
+                    While reader.Read()
+                        dgvProducts.Rows.Add(
+                            Int(reader("product_id")),
+                            reader("category").ToString(),
+                            reader("item_name").ToString(),
+                            reader("size").ToString(),
+                            (FormatCurrency(reader("price")).Replace("$", "₱ "))
+                        )
+                    End While
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Sub GetAllOrderDetails()
+
+        Dim connectionString As String = "Data Source=DESKTOP-FOAVQMH\SQLEXPRESS; Initial Catalog=FLOATS_POS_SYSTEM; Integrated Security=True; TrustServerCertificate=True;"
+
+        Dim selectQuery As String = "SELECT order_id, product_id, quantity, total_price, date_ordered, time_ordered FROM OrderDetails"
+
+        Using connection As New SqlConnection(connectionString)
+            connection.Open()
+
+            Using command As New SqlCommand(selectQuery, connection)
+                Using reader As SqlDataReader = command.ExecuteReader()
+                    dgvOrderDetails.Rows.Clear()
+
+                    While reader.Read()
+                        dgvOrderDetails.Rows.Add(
+                            reader("order_id").ToString(),
+                            reader("product_id").ToString(),
+                            reader("quantity").ToString(),
+                            (FormatCurrency(reader("total_price")).Replace("$", "₱ ")),
+                            CDate(reader("date_ordered")).ToString("MMMM d, yyyy"),
+                            DateTime.Today.Add(CType(reader("time_ordered"), TimeSpan)).ToString("h:mm tt")
+                        )
+                    End While
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Sub SortAllOrderDetails()
+
+        Dim connectionString As String = "Data Source=DESKTOP-FOAVQMH\SQLEXPRESS; Initial Catalog=FLOATS_POS_SYSTEM; Integrated Security=True; TrustServerCertificate=True;"
+
+        Dim selectQuery As String
+
+        If (sortAscending) Then
+            selectQuery = "SELECT order_id, product_id, quantity, total_price, date_ordered, time_ordered FROM OrderDetails ORDER BY order_id ASC"
+        Else
+            selectQuery = "SELECT order_id, product_id, quantity, total_price, date_ordered, time_ordered FROM OrderDetails ORDER BY order_id DESC"
+        End If
+
+        Using connection As New SqlConnection(connectionString)
+            connection.Open()
+
+            Using command As New SqlCommand(selectQuery, connection)
+                Using reader As SqlDataReader = command.ExecuteReader()
+                    dgvOrderDetails.Rows.Clear()
+
+                    While reader.Read()
+                        dgvOrderDetails.Rows.Add(
+                            reader("order_id").ToString(),
+                            reader("product_id").ToString(),
+                            reader("quantity").ToString(),
+                            (FormatCurrency(reader("total_price")).Replace("$", "₱ ")),
+                            CDate(reader("date_ordered")).ToString("MMMM d, yyyy"),
+                            DateTime.Today.Add(CType(reader("time_ordered"), TimeSpan)).ToString("h:mm tt")
+                        )
+                    End While
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Sub dgvProducts_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvProducts.CellContentClick
+
+    End Sub
+
+    Private Sub dgvOrderDetails_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvOrderDetails.CellContentClick
+
+    End Sub
+
+    Private Sub btn_products_Click(sender As Object, e As EventArgs) Handles btn_products.Click
+        dgvOrderDetails.Visible = False
+        dgvProducts.Visible = True
+
+        currentTable = dgvProducts
+        GetAllProducts()
+    End Sub
+
+    Private Sub btn_orders_Click(sender As Object, e As EventArgs) Handles btn_orders.Click
+        dgvOrderDetails.Visible = True
+        dgvProducts.Visible = False
+
+        currentTable = dgvOrderDetails
+        GetAllOrderDetails()
+    End Sub
+
+    Private Sub btn_sort_Click(sender As Object, e As EventArgs) Handles btn_sort.Click
+
+        If sortAscending Then
+            btn_sort.Text = "🔽 Sort"
+            If currentTable Is dgvProducts Then
+                SortAllProducts()
+            Else
+                SortAllOrderDetails()
+            End If
+        Else
+            btn_sort.Text = "🔼 Sort"
+            If currentTable Is dgvProducts Then
+                SortAllProducts()
+            Else
+                SortAllOrderDetails()
+            End If
+        End If
+
+        sortAscending = Not sortAscending
+    End Sub
+
+    Private Sub tb_search_TextChanged(sender As Object, e As EventArgs) Handles tb_search.TextChanged
+        Dim searchText As String = tb_search.Text.ToLower()
+
+        For Each row As DataGridViewRow In dgvProducts.Rows
+            row.Visible = row.Cells.Cast(Of DataGridViewCell)().
+                Any(Function(cell) cell.Value IsNot Nothing AndAlso
+                    cell.Value.ToString().ToLower().Contains(searchText))
+        Next
+
+        For Each row As DataGridViewRow In dgvOrderDetails.Rows
+            row.Visible = row.Cells.Cast(Of DataGridViewCell)().
+                Any(Function(cell) cell.Value IsNot Nothing AndAlso
+                    cell.Value.ToString().ToLower().Contains(searchText))
+        Next
+    End Sub
+
+    Private Sub btn_remove_Click(sender As Object, e As EventArgs) Handles btn_remove.Click
+        If dgvOrderDetails.SelectedRows.Count > 0 Then
+            ' Get the primary key (e.g., order_id) from the selected row
+            Dim orderID As Integer = Convert.ToInt32(dgvOrderDetails.SelectedRows(0).Cells("orderID").Value)
+
+            ' Confirm delete
+            If CustomMessageBox.Show("Are you sure?", "CONFIRMATION", "YES/NO") = DialogResult.Yes Then
+                Dim connectionString As String = "Data Source=DESKTOP-FOAVQMH\SQLEXPRESS; Initial Catalog=FLOATS_POS_SYSTEM; Integrated Security=True; TrustServerCertificate=True;"
+                Using connection As New SqlConnection(connectionString)
+                    connection.Open()
+
+                    Dim deleteQuery As String = "DELETE FROM OrderDetails WHERE order_id = @order_id"
+                    Using command As New SqlCommand(deleteQuery, connection)
+                        command.Parameters.AddWithValue("@order_id", orderID)
+                        command.ExecuteNonQuery()
+                    End Using
+                End Using
+
+                If currentTable Is dgvProducts Then
+                    SortAllProducts()
+                Else
+                    SortAllOrderDetails()
+                End If
+            End If
+        Else
+            CustomMessageBox.Show("Please select a row to delete.", "ERROR", "OK")
+        End If
+    End Sub
+End Class
